5563703e46b05099daf300afde56eae22d095548
tpescatore <twp15@my.fsu.edu>
2019-10-15 13:04:03 -0400

    debug

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-exports.ACCESS_TOKEN = "GroupMe access token";
-exports.BOT_ID = "GroupMe bot ID";
-exports.GROUP_ID = "GroupMe group id";
-exports.GIHPY_KEY = 'Giphy API key';
+exports.ACCESS_TOKEN = "Los1bE7JCfqfe5OvDJlwYRqu5wLmm6esXiaA6qRm";
+exports.BOT_ID = "80bf35c3597d105a736c3893c2";
+exports.GROUP_ID = "48845062";
+exports.GIHPY_KEY = 'AVJuWHdPHyGTngDoOx6G1kW46ZrJfya3';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 
