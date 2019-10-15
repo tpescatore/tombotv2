4c88521c0200933c67d7d84bd83f64d8f1bfe765
tpescatore <tommypescatore@gmail.com>
2019-10-15 14:46:40 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 exports.ACCESS_TOKEN = "Los1bE7JCfqfe5OvDJlwYRqu5wLmm6esXiaA6qRm";
 exports.BOT_ID = "095bd83048b4af78f9aaac090c";
-exports.GROUP_ID = "	54896441";
+exports.GROUP_ID = "54896441";
 exports.GIHPY_KEY = 'AVJuWHdPHyGTngDoOx6G1kW46ZrJfya3';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
